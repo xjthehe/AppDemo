1052c3b33f7d6197ab3343206dc159454a234432
617490820@qq.com <5070363>
2017-10-27 11:08:47 +0800

    初始导入11

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lenovo.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //----------
+        Log.e("xxx","xxx");
     }
 }
